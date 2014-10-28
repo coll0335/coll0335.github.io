a30f825e84291cc1f4bb3ffb5da8cdd85795cfff
Mark Collantes <collantes.mark.mgc@gmail.com>
2014-10-28 16:12:54 -0400

    WIP on Portfolio Section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var hamburgerToggle = $('.hb-logo');
 var sideNav = $ ('.side-nav');
+var sideNavHome = $('.sn-home');
+var sideNavWork = $('.sn-work');
+var sideNavAbout = $('.sn-about');
+var sideNavContact = $('.sn-contact');
 
 var sideNavClickHandler = function (e) {
 	var isActive = sideNav.attr('data-state');
@@ -13,3 +17,20 @@ var sideNavClickHandler = function (e) {
 };
 
 hamburgerToggle.on('click', sideNavClickHandler);
+
+
+var sideNavItemClick = function () {
+	var isActive = sideNav.attr('data-state');
+
+	if (isActive == 'active') {
+		sideNav.attr('data-state','inactive');
+	} else {
+		sideNav.attr('data-state','active');
+	}
+};
+
+sideNavHome.on('click', sideNavItemClick);
+sideNavWork.on('click', sideNavItemClick);
+sideNavAbout.on('click', sideNavItemClick);
+sideNavContact.on('click', sideNavItemClick);
+
