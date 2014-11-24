b8bc8c0acaa0176e8a601e5aaef684ab2ee21fce
Mark Collantes <collantes.mark.mgc@gmail.com>
2014-11-24 17:04:39 -0500

    Added javascript to main
    Added scroll animation script

--- js/main.js
+++ js/main.js
@@ -57,6 +57,37 @@ $( ".sn-contact" ).on( "click", function() {
 });
 
 
+// $(document).ready(function() {
+// 	$(".sn-container li a").serialScroll();
+// });
+
+var hashTagActive = "";
+$(document).ready( function() {
+	$(".smooth-scroll li a").click(function (event) {
+
+		if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+			event.preventDefault();
+
+			//calculate destination place
+			var dest = 0;
+			if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
+				dest = $(document).height() - $(window).height();
+			}
+			else {
+				dest = $(this.hash).offset().top;
+			}
+
+			//go to destination
+			$('html, body').animate({
+			    scrollTop: dest
+			}, 700, 'swing');
+
+			hashTagActive = this.hash;
+		}
+
+	});
+});
+
 
 // Masonry
 
